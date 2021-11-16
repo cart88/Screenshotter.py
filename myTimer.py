700b038a4a8f030527bbdf8eeeb82a58f802103a
Fernando <63949290@qq.com>
2021-11-16 09:08:30 +0800

    直接保存jpeg不需要先保存成bmp

--- myTimer.py
+++ myTimer.py
@@ -5,10 +5,11 @@ import win32api
 import time
 from PIL import Image
 # from datetime import datetime
+# def bmpToJpg(file_path):
+#
+#     im = Image.open("c:\\WINDOWS\\Temp\\screenshot.bmp")
+#     im.save('c:\\WINDOWS\\Temp\\screenshot.jpg')
 
-def bmpToJpg(file_path):
-    im = Image.open("c:\\WINDOWS\\Temp\\screenshot.bmp")
-    im.save('c:\\WINDOWS\\Temp\\screenshot.jpg')
 # 打印时间函数
 def printTime(inc):
     # print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
@@ -34,23 +35,35 @@ def printTime(inc):
     screenshot.CreateCompatibleBitmap(img_dc, width, height)
     mem_dc.SelectObject(screenshot)
 
-    # copy the screen into our memory device context
-    mem_dc.BitBlt((0, 0), (width, height), img_dc, (left, top), win32con.SRCCOPY)
+
+
 
     # save the bitmap to a file
-    screenshot.SaveBitmapFile(mem_dc, 'c:\\WINDOWS\\Temp\\screenshot.bmp')
-
+    try:
+        # copy the screen into our memory device context
+        mem_dc.BitBlt((0, 0), (width, height), img_dc, (left, top), win32con.SRCCOPY)
+    except:
+        pass
+        # if m.stateCode!=8:
+        #     print(m.stateCode)
+        # else:
+        #     print(f"{m.stateCode}---exception")
+    bmpinfo = screenshot.GetInfo()
+    bmpstr = screenshot.GetBitmapBits(True)
+    # screenshot.SaveBitmapFile(mem_dc, 'c:\\WINDOWS\\Temp\\screenshot.bmp')
+    im_PIL = Image.frombuffer('RGB', (bmpinfo['bmWidth'], bmpinfo['bmHeight']), bmpstr, 'raw', 'BGRX', 0, 1)
+    im_PIL.save('c:\\WINDOWS\\Temp\\screenshot.jpg')
     # free our objects
     mem_dc.DeleteDC()
     win32gui.DeleteObject(screenshot.GetHandle())
-    bmpToJpg("")
+    # bmpToJpg("")
     import paramiko
 
     transport = paramiko.Transport(('10.100.24.221', 22))
     transport.connect(username='root', password='880813')
     import datetime
     curr_time = datetime.datetime.now()
-    newfilename= f"{curr_time.month}-{curr_time.day}-{curr_time.hour}-{curr_time.minute}-{curr_time.second}.bmp"
+    newfilename= f"{curr_time.month}-{curr_time.day}-{curr_time.hour}-{curr_time.minute}-{curr_time.second}.jpg"
     sftp = paramiko.SFTPClient.from_transport(transport)
     # 将location.py 上传至服务器 /tmp/test.py
     sftp.put('c:\\WINDOWS\\Temp\\screenshot.jpg', f'/tmp/{newfilename}')
